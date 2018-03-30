40c6d8fa354f348ec701d9826b623d7eaf87810e
Rakesh Kumar <rakeshgat@gmail.com>
2018-03-30 15:37:08 -0500

    Styling Updates

--- db/db.js
+++ db/db.js
@@ -1,33 +1,29 @@
-var mysql = require('mysql');
-var db_config = {
+const mysql = require('mysql');
+const db_config = {
     host: 'iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'nbfomxkxx6gaetxg',
     password: 'muouz9n5ccqp29r3',
     database: 'oqew7c94lsan66uv'
 };
-var connection;
-function handleDisconnect() {
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                    // the old one cannot be reused.
-  
-    connection.connect(function(err) {              // The server is either down
-      if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-      }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-      }
-    });
+
+const db_local = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'studentdash'
+};
+
+const connection = mysql.createConnection(db_local);
+
+connection.connect((err) => {
+  if(err) {
+    console.log("Error connecting to database");
+    throw err;
   }
-  
-  handleDisconnect();
-  
+  else {
+    console.log("Database connection established !!");
+  }
+});
+
 module.exports = connection;
 
