7d7fc1973cd7aa9e8766e506c86b0045a8945279
Rakesh Kumar <rakeshgat@gmail.com>
2018-03-29 23:42:24 -0500

    Initial Commit

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'studentdash'
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b1815fb31e8ae5',
+    password: '073c96f1',
+    database: 'heroku_d64d553307ea5a4'
 });
 
 connection.connect(function(err) {
